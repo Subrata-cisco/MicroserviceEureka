d0b78cb8e798eeddf19a35230553cf65ff34cb67
subrsaha <subrsaha@cisco.com>
2017-08-16 20:50:36 +0530

    Added 2 service :  PIG, MIG
    Added 2 service :  PIG, MIG

---
+++ MIG/src/main/java/com/stealthmode/service/mig/MigApplication.java
@@ -0,0 +1,12 @@
+package com.stealthmode.service.mig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigApplication.class, args);
+	}
+}
---
+++ PIG/src/main/java/com/stealthmode/service/pig/PigApplication.java
@@ -0,0 +1,12 @@
+package com.stealthmode.service.pig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigApplication.class, args);
+	}
+}
