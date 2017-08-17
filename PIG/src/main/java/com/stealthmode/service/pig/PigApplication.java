a2b334b70786596c2772b577ad1db001761dd4fe
subrsaha <subrsaha@cisco.com>
2017-08-17 22:26:41 +0530

    property file checked in
    property file checked in

--- PIG/src/main/java/com/stealthmode/service/pig/PigApplication.java
+++ PIG/src/main/java/com/stealthmode/service/pig/PigApplication.java
@@ -2,11 +2,26 @@ package com.stealthmode.service.pig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RefreshScope
+@EnableEurekaClient
 public class PigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PigApplication.class, args);
-	}
+    //@Value("${hello.pig}")
+    private String message;
+
+    @RequestMapping("/msg")
+    public String getMessage(){
+        return this.message;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigApplication.class, args);
+    }
 }
