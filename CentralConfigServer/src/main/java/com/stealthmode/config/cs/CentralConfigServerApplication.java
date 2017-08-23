918f91cb1a3e70c9be2ecde8988f08f514bdd6bf
subrsaha <subrsaha@cisco.com>
2017-08-23 23:10:01 +0530

    Config server and Eureka server

---
+++ CentralConfigServer/src/main/java/com/stealthmode/config/cs/CentralConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.stealthmode.config.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CentralConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralConfigServerApplication.class, args);
+    }
+}
---
+++ CentralEurekaServer/src/main/java/com/stealthmode/registry/es/CentralEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.stealthmode.registry.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CentralEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralEurekaServerApplication.class, args);
+    }
+}
